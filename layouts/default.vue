b94f68bd17a02dba18f5635f3045ee116ca8c6ba
Bravco <boss.wiw@gmail.com>
2025-06-21 21:07:32 +0200

    Made layout and homepage without using nuxt ui pro

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,36 +1,55 @@
 <template>
     <div>
-        <UHeader mode="slideover">
-          <template #title>
-            <h1><span class="text-primary">SMC</span> Docs</h1>
-          </template>
+      <header class="bg-default/75 backdrop-blur border-b border-default h-(--ui-header-height) sticky top-0 z-50">
+        <nav class="max-w-(--ui-container) h-full mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between gap-3">
+          <NuxtLink to="/" class="lg:flex-1">
+            <h1 class="text-xl font-bold text-highlighted"><span class="text-primary">SMC</span> Docs</h1>
+          </NuxtLink>
 
-          <UNavigationMenu :items="items" variant="link"/>
-
-          <template #body>
-            <UNavigationMenu :items="items" orientation="vertical"/>
-          </template>
-
-          <template #right>
+          <UNavigationMenu class="hidden lg:flex" :items="items" variant="link"/>
+          
+          <div class="flex items-center justify-end lg:flex-1 gap-1.5">
             <ColorModeButton/>
-          </template>
-        </UHeader>
+            <USlideover v-model:open="open">
+              <UButton :icon="open ? 'i-lucide-x' : 'i-lucide-menu'" variant="ghost" color="neutral" class="lg:hidden -me-1.5"/>
 
-        <UMain>
-          <UContainer>
-            <slot/>
-          </UContainer>
-        </UMain>
+              <template #header>
+                <div class="w-full flex items-center justify-between">
+                  <NuxtLink to="/" class="lg:flex-1">
+                    <h1 class="text-xl font-bold text-highlighted"><span class="text-primary">SMC</span> Docs</h1>
+                  </NuxtLink>
+                  <div>
+                    <ColorModeButton/>
+                    <UButton @click="open = false" icon="i-lucide-x" variant="ghost" color="neutral"/>
+                  </div>
+                </div>
+              </template>
 
-        <USeparator/>
-        <UFooter>
-          <p class="text-center text-sm text-muted font-medium">Copyright &copy; 2025 | All Rights Reserved.</p>
-        </UFooter>
+              <template #body>
+                <UNavigationMenu @click="open = false" :items="items" orientation="vertical"/>
+              </template>
+            </USlideover>
+          </div>
+        </nav>
+      </header>
+
+      <main class="min-h-[calc(100vh-var(--ui-header-height))]">
+        <UContainer>
+          <slot/>
+        </UContainer>
+      </main>
+
+      <USeparator/>
+      <footer class="py-8">
+        <p class="text-center text-sm text-muted">Copyright &copy; 2025 | All Rights Reserved.</p>
+      </footer>
     </div>
 </template>
 
 <script lang="ts" setup>
   const route = useRoute();
+
+  const open = ref<boolean>(false);
 
   const items = computed<NavigationMenuItem[]>(() => [
     {
@@ -46,4 +65,6 @@
       active: route.path !== "/"
     }
   ]);
+
+  watch(() => route.fullPath, () => open.value = false);
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,97 +1,120 @@
 <template>
-    <UPage>
-        <UPageHero headline="For Free" :links="links">
-            <template #title>
-                <h1><span class="text-primary">Smart Money Concepts</span> Docs</h1>
-            </template>
-            <template #description>
-                <p>Stop trading like the <span class="font-medium">dumb money</span> — start thinking like the <span class="font-medium">smart money</span>.</p>
-            </template>
-        </UPageHero>
+    <div>
+        <section>
+            <UContainer class="flex flex-col lg:grid py-24 sm:py-32 lg:py-40 gap-16 sm:gap-y-24">
+                <div class="text-center">
+                    <UBadge icon="i-lucide-rocket" label="For Free" variant="soft" color="success"/>
+                    <h1 class="text-5xl sm:text-7xl text-pretty tracking-tight font-bold text-highlighted"><span class="text-primary">Smart Money Concepts</span> Docs</h1>
+                    <p class="text-lg sm:text-xl/8 text-muted text-balance mt-6">Stop trading like the dumb money — start thinking like the smart money.</p>
+                    <div class="mt-8 flex flex-wrap gap-x-6 gap-y-3 justify-center">
+                        <UButton to="/candle-behaviour" label="Get started" icon="i-lucide-wand-sparkles"/>
+                        <UButton to="/#about" label="Learn more" trailingIcon="i-lucide-arrow-right" variant="subtle" color="neutral"/>
+                    </div>
+                </div>
+            </UContainer>
+        </section>
 
-        <UPageSection id="about" description="Learn price action and proven liquidity concepts that actually work.">
-            <template #title>
-                <h2>Everything to become <span class="text-primary">profitable</span></h2>
-            </template>
-            <UPageGrid>
-                <UPageCard
-                    v-for="(card, index) in cards"
-                    :key="index"
-                    v-bind="card"
-                    :spotlight="true"
-                    variant="subtle"
-                >
-                    <UColorModeImage
-                        v-if="card.image"
-                        :light="`${card.image.path}-light.svg`"
-                        :dark="`${card.image.path}-dark.svg`"
-                        :width="card.image.width"
-                        :height="card.image.height"
-                        :alt="card.title"
-                        loading="lazy"
-                        class="w-full"
-                    />
-                </UPageCard>
-            </UPageGrid>
-        </UPageSection>
+        <section id="about">
+            <UContainer class="flex flex-col lg:grid py-16 sm:py-24 lg:py-32 gap-8 sm:gap-16">
+                <div>
+                    <h2 class="text-3xl sm:text-4xl lg:text-5xl text-pretty tracking-tight font-bold text-highlighted text-center">Everything to become <span class="text-primary">profitable</span></h2>
+                    <p class="text-base sm:text-lg text-muted text-center text-balance mt-6">Learn price action and proven liquidity concepts that actually work.</p>
+                </div>
+                <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+                    <UCard
+                        v-for="(card, index) in cards"
+                        :key="index"
+                        :class="[card.class]"
+                        variant="subtle"
+                    >
+                        <div 
+                            class="flex flex-col flex-1"
+                            :class="card.image ? 'lg:grid gap-x-8 gap-y-4 lg:grid-cols-2 lg:items-center' : 'items-start'"
+                        >
+                            <div class="h-full flex flex-col flex-1 items-start">
+                                <UIcon
+                                    :name="card.icon.name"
+                                    :class="['size-5  mb-2.5', `text-${card.icon.color}`]"
+                                />
+                                <div class="flex-1">
+                                    <h3 class="text-base text-pretty font-semibold text-highlighted">{{ card.title }}</h3>
+                                    <p class="text-[15px] text-pretty text-toned mt-1">{{ card.description }}</p>
+                                </div>
+                            </div>
+                            <ClientOnly>
+                                <NuxtImg
+                                    v-if="card.image"
+                                    :src="`${card.image.path}-${colorMode.value}.svg`"
+                                    :width="card.image.width"
+                                    :height="card.image.height"
+                                    :alt="card.title"
+                                    loading="lazy"
+                                    class="w-full"
+                                />
+                            </ClientOnly>
+                        </div>
+                    </UCard>
+                </div>
+            </UContainer>
+        </section>
 
-        <UPageSection>
-            <UPageCTA variant="subtle" :links="links">
-                <template #title>
-                    <h2>Backed by <span class="text-primary">profitable traders</span></h2>
-                </template>
-                <template #description>
-                    <p>95% of traders fail because they do not trade <span class="font-medium">Smart Money Concepts</span>.</p>
-                </template>
-            </UPageCTA>
-        </UPageSection>
-    </UPage>
+        <section>
+            <UContainer class="py-16 sm:py-24 lg:py-32">
+                <div class="rounded-xl bg-elevated/50 ring ring-default px-6 py-12 sm:px-12 sm:py-24 lg:px-16 lg:py-24">
+                    <h2 class="text-3xl sm:text-4xl text-pretty tracking-tight font-bold text-highlighted text-center">Backed by <span class="text-primary">profitable traders</span></h2>
+                    <p class="text-base sm:text-lg text-center text-balance text-toned mt-6">95% of traders fail because they do not trade <span class="font-bold">Smart Money Concepts</span>.</p>
+                    <div class="mt-8 flex flex-wrap gap-x-6 gap-y-3 justify-center">
+                        <UButton to="/candle-behaviour" label="Get started" icon="i-lucide-wand-sparkles"/>
+                        <UButton to="/#about" label="Learn more" trailingIcon="i-lucide-arrow-right" variant="subtle" color="neutral"/>
+                    </div>
+                </div>
+            </UContainer>
+        </section>
+    </div>
 </template>
 
 <script lang="ts" setup>
-    const links = ref([
-        {
-            label: "Get started",
-            icon: "i-lucide-wand-sparkles",
-            to: "/candle-behaviour"
-        },
-        {
-            label: "Learn more",
-            to: "/#about",
-            color: "neutral",
-            variant: "subtle",
-            trailingIcon: "i-lucide-arrow-right"
-        }
-    ]);
+    const colorMode = useColorMode();
 
     const cards = ref([
         {
             title: "Liquidity",
             description: "Market movement is driven by liquidity and nothing else.",
-            icon: "i-lucide-glass-water",
+            icon: {
+                name: "i-lucide-glass-water",
+                color: "info"
+            },
             class: "lg:col-span-2",
             image: {
                 path: "/images/liquidity",
                 width: 240,
                 height: 135
             },
-            orientation: "horizontal" as const,
-            
+            orientation: "horizontal" as const
         },
         {
             title: "Strategy Creation",
             description: "Combine your trading knowledge into a money-making system.",
-            icon: "i-lucide-wrench"
+            icon: {
+                name: "i-lucide-wrench",
+                color: "warning"
+            }
         },
         {
             title: "Psychology",
             description: "Most traders fail because they cannot control their emotions.",
-            icon: "i-lucide-brain"
+            icon: {
+                name: "i-lucide-brain",
+                color: "success"
+            }
         },
         {
             title: "Market Structure",
             description: "Reading current price action and determining the trend are mandatory while trading.",
-            icon: "i-lucide-chart-candlestick",
+            icon: {
+                name: "i-lucide-chart-candlestick",
+                color: "error"
+            },
             class: "lg:col-span-2",
             image: {
                 path: "/images/trends",

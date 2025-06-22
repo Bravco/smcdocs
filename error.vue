d3e27a7af9d57996a02f1be8bde2a8b338ab5e2e
Bravco <boss.wiw@gmail.com>
2025-06-22 13:18:39 +0200

    Added error page without nuxt ui pro

--- error.vue
+++ error.vue
@@ -1,6 +1,13 @@
 <template>
     <UApp>
-        <UError :error="error"/>
+        <main class="min-h-[calc(100vh-var(--ui-header-height))] flex flex-col items-center justify-center text-center">
+            <span class="text-base font-semibold text-primary">{{ error.statusCode }}</span>
+            <h1 class="mt-2 text-4xl sm:text-5xl font-bold text-highlighted text-balance">{{ error.statusMessage }}</h1>
+            <p class="mt-4 text-lg text-muted text-balance">{{ error.message }}</p>
+            <div class="mt-8 flex items-center justify-center gap-6">
+                <UButton to="/" label="Back to home"/>
+            </div>
+        </main>
     </UApp>
 </template>
 
